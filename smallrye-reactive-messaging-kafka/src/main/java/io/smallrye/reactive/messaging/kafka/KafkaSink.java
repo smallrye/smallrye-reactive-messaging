709307cd59cfb6d75eb56a1501cf3ec9e0417679
Clement Escoffier <clement.escoffier@gmail.com>
2018-10-07 10:30:08 +0200

    Support Kafka sink without topic - the topic is set in the message itself.
    Messages without topic are discarded

--- smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/KafkaSink.java
+++ smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/KafkaSink.java
@@ -7,6 +7,8 @@ import io.vertx.kafka.client.producer.KafkaWriteStream;
 import io.vertx.kafka.client.producer.RecordMetadata;
 import io.vertx.reactivex.core.Vertx;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.microprofile.reactive.messaging.Message;
 import org.eclipse.microprofile.reactive.streams.ReactiveStreams;
 import org.reactivestreams.Subscriber;
@@ -20,6 +22,7 @@ import java.util.concurrent.CompletionStage;
 import static io.smallrye.reactive.messaging.spi.ConfigurationHelper.*;
 
 public class KafkaSink {
+  private static final Logger LOGGER = LogManager.getLogger(KafkaSink.class);
   private final KafkaWriteStream stream;
   private final int partition;
   private final Optional<Long> timestamp;
@@ -33,13 +36,22 @@ public class KafkaSink {
     partition = conf.getAsInteger("partition", 0);
     timestamp = conf.getAsLong( "timestamp");
     key = conf.get("key");
-    topic = conf.getOrDie("topic");
+    topic = conf.get("topic");
+    if (topic == null) {
+      LOGGER.warn("No default topic configured, only sending messages with an explicit topic set");
+    }
 
     subscriber = ReactiveStreams.<Message>builder()
       .flatMapCompletionStage(message -> {
         ProducerRecord record;
         if (message instanceof  KafkaMessage) {
           KafkaMessage km = ((KafkaMessage) message);
+
+          if (this.topic == null  && ((KafkaMessage) message).getTopic() == null) {
+            LOGGER.error("Ignoring message - no topic set");
+            return CompletableFuture.completedFuture(null);
+          }
+
           record = new ProducerRecord<>(
             km.getTopic() == null ? this.topic : km.getTopic(),
             km.getPartition() == null ? this.partition : km.getPartition(),
@@ -48,21 +60,28 @@ public class KafkaSink {
             km.getPayload(),
             km.getHeaders().unwrap()
           );
+          LOGGER.info("Sending message {} to Kafka topic '{}'", message, record.topic());
         } else {
+          if (this.topic == null) {
+            LOGGER.error("Ignoring message - no topic set");
+            return CompletableFuture.completedFuture(null);
+          }
           record
             = new ProducerRecord<>(topic, partition, timestamp.orElse(null), key, message.getPayload());
         }
 
         CompletableFuture<RecordMetadata> future = new CompletableFuture<>();
-
         Handler<AsyncResult<RecordMetadata>> handler = ar -> {
           if (ar.succeeded()) {
+            LOGGER.info("Message {} sent successfully to Kafka topic {}", message, record.topic());
             future.complete(ar.result());
           } else {
+            LOGGER.error("Message {} was not sent to Kafka topic {}", message, record.topic(), ar.cause());
             future.completeExceptionally(ar.cause());
           }
         };
 
+        LOGGER.debug("Using stream {} to write the record {}", stream, record);
         stream.write(record, handler);
         return future;
       }).ignore().build();
