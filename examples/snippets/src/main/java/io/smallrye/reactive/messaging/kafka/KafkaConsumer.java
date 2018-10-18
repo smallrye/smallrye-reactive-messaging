2ff552fd991170a9edf229523efb8436a9c2e1ce
Clement Escoffier <clement.escoffier@gmail.com>
2018-10-18 16:27:14 +0200

    Initial version of the documentation

---
+++ examples/snippets/src/main/java/io/smallrye/reactive/messaging/kafka/KafkaConsumer.java
@@ -0,0 +1,19 @@
+package io.smallrye.reactive.messaging.kafka;
+
+import io.vertx.core.json.JsonObject;
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+public class KafkaConsumer {
+
+  // tag::kafka-message[]
+  @Incoming("from-kafka")
+  public void consume(KafkaMessage<String, JsonObject> message) {
+    JsonObject payload = message.getPayload();
+    String key = message.getKey();
+    MessageHeaders headers = message.getHeaders();
+    Integer partition = message.getPartition();
+    Long timestamp = message.getTimestamp();
+  }
+  // end::kafka-message[]
+
+}
