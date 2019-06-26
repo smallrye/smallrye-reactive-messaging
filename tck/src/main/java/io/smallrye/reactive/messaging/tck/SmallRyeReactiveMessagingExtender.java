7c33a9bc6ec56e7319fe7b99e5bfbb113935dbbe
Clement Escoffier <clement.escoffier@gmail.com>
2019-06-26 14:37:06 +0200

    Fix #144
    * Update specification version to 1.0-RC2
    * Remove the embedded version fo the API
    * Make the TCK run as part of the build

--- tck/src/main/java/io/smallrye/reactive/messaging/tck/SmallRyeReactiveMessagingExtender.java
+++ tck/src/main/java/io/smallrye/reactive/messaging/tck/SmallRyeReactiveMessagingExtender.java
@@ -1,20 +1,20 @@
 package io.smallrye.reactive.messaging.tck;
 
-import io.smallrye.reactive.messaging.ChannelRegistry;
-import io.smallrye.reactive.messaging.extension.ReactiveMessagingExtension;
+import javax.enterprise.inject.spi.Extension;
+
 import org.eclipse.microprofile.reactive.messaging.tck.ArchiveExtender;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
-import javax.enterprise.inject.spi.Extension;
+import io.smallrye.reactive.messaging.ChannelRegistry;
+import io.smallrye.reactive.messaging.extension.ReactiveMessagingExtension;
 
 public class SmallRyeReactiveMessagingExtender implements ArchiveExtender {
-  @Override
-  public void extend(JavaArchive archive) {
-    archive
-      .addPackages(true, ChannelRegistry.class.getPackage())
-      .addAsServiceProvider(Extension.class, ReactiveMessagingExtension.class)
-      .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-  }
+    @Override
+    public void extend(JavaArchive archive) {
+        archive
+                .addPackages(true, ChannelRegistry.class.getPackage())
+                .addAsServiceProvider(Extension.class, ReactiveMessagingExtension.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
 }
-
