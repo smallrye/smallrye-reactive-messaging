0dbad6b4b77a432e967d9efe4594290da5da1431
Minto van der Sluis <minto@xup.nl>
2019-06-26 11:33:46 +0200

    First take on amqp example

---
+++ examples/amqp-quickstart/src/main/java/acme/BeanUsingAnEmitter.java
@@ -0,0 +1,29 @@
+package acme;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.smallrye.reactive.messaging.annotations.Emitter;
+import io.smallrye.reactive.messaging.annotations.Stream;
+
+@ApplicationScoped
+public class BeanUsingAnEmitter {
+
+    @Inject
+    @Stream("my-stream")
+    Emitter<String> emitter;
+
+    public void periodicallySendMessage() {
+        AtomicInteger counter = new AtomicInteger();
+        Executors.newSingleThreadScheduledExecutor()
+                .scheduleAtFixedRate(() -> {
+                    emitter.send("Hello " + counter.getAndIncrement());
+                },
+                        1, 1, TimeUnit.SECONDS);
+    }
+
+}
---
+++ examples/amqp-quickstart/src/main/java/acme/Sender.java
@@ -0,0 +1,30 @@
+package acme;
+
+import java.util.concurrent.*;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import io.smallrye.reactive.messaging.amqp.AmqpMessage;
+
+@ApplicationScoped
+public class Sender {
+
+    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+
+    @Outgoing("data")
+    public CompletionStage<AmqpMessage> send() {
+        CompletableFuture<AmqpMessage> future = new CompletableFuture<>();
+        delay(() -> {
+            System.out.println("Sending message on dynamic topic: hello");
+            future.complete(new AmqpMessage("hello from sender"));
+        });
+        return future;
+    }
+
+    private void delay(Runnable runnable) {
+        executor.schedule(runnable, 5, TimeUnit.SECONDS);
+    }
+
+}
