66371b5559b653521ef4ae4df789e2abef4c911f
Clement Escoffier <clement.escoffier@gmail.com>
2019-09-22 09:48:49 +0200

    disable SNS test on linux

--- smallrye-reactive-messaging-aws-sns/src/test/java/io/smallrye/reactive/messaging/aws/sns/AwsSnsTest.java
+++ smallrye-reactive-messaging-aws-sns/src/test/java/io/smallrye/reactive/messaging/aws/sns/AwsSnsTest.java
@@ -22,96 +22,105 @@ import io.restassured.RestAssured;
 
 public class AwsSnsTest extends AwsSnsTestBase {
 
-    private WeldContainer container;
-    private String topic = "sns-test";
+  private WeldContainer container;
+  private String topic = "sns-test";
 
-    @Before
-    public void initTest() {
-        System.setProperty("aws.region", "some-region");
-        System.setProperty("aws.accessKeyId", "some-key-id");
-        System.setProperty("aws.secretAccessKey", "some-access-id");
-        Weld weld = baseWeld();
-        weld.addBeanClass(ConsumptionBean.class);
-        addConfig(createSourceConfig(topic));
+  @Before
+  public void initTest() {
+    System.setProperty("aws.region", "some-region");
+    System.setProperty("aws.accessKeyId", "some-key-id");
+    System.setProperty("aws.secretAccessKey", "some-access-id");
+    Weld weld = baseWeld();
+    weld.addBeanClass(ConsumptionBean.class);
+    addConfig(createSourceConfig(topic));
 
-        container = weld.initialize();
+    container = weld.initialize();
 
-        await()
-                .pollDelay(1, TimeUnit.SECONDS)
-                .until(() -> {
-                    try {
-                        URL url = new URL("http://localhost:" + 8389);
-                        RestAssured.head(url).then().statusCode(204);
-                        return true;
-                    } catch (Exception e) {
-                        return false;
-                    }
-                });
-    }
-
-    @After
-    public void clearTest() {
-        System.clearProperty("aws.region");
-        System.clearProperty("aws.accessKeyId");
-        System.clearProperty("aws.secretAccessKey");
-
-        clear();
-        container.shutdown();
-    }
-
-    @Test
-    public void testSourceAndSink() {
-        ConsumptionBean bean = container.select(ConsumptionBean.class).get();
-        send("Hello-0", topic);
-        await().until(() -> bean.messages().size() == 1);
-        assertThat(bean.messages().get(0)).isEqualTo("Hello-0");
-        for (int i = 1; i < 11; i++) {
-            send("Hello-" + i, topic);
+    await()
+      .pollDelay(1, TimeUnit.SECONDS)
+      .until(() -> {
+        try {
+          URL url = new URL("http://localhost:" + 8389);
+          RestAssured.head(url).then().statusCode(204);
+          return true;
+        } catch (Exception e) {
+          return false;
         }
-        await().until(() -> bean.messages().size() == 11);
-        assertThat(bean.messages()).allSatisfy(s -> assertThat(s).startsWith("Hello"));
-    }
+      });
+  }
 
-    private MapBasedConfig createSourceConfig(String topic) {
-        String prefix = "mp.messaging.incoming.source.";
-        Map<String, Object> config = new HashMap<>();
-        config.put(prefix.concat("connector"), SnsConnector.CONNECTOR_NAME);
-        config.put(prefix.concat("channel"), topic.concat("-transformed"));
-        String appUrl = getAppUrl();
-        int port = 8389;
-        config.put(prefix.concat("port"), port);
-        config.put("sns-app-host", appUrl);
-        config.put("mock-sns-topics", true);
-        config.put("sns-url", String.format("http://%s:%d", ip(), port()));
-        return new MapBasedConfig(config);
-    }
+  @After
+  public void clearTest() {
+    System.clearProperty("aws.region");
+    System.clearProperty("aws.accessKeyId");
+    System.clearProperty("aws.secretAccessKey");
 
-    private String getAppUrl() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.contains("win")) {
-            throw new UnsupportedOperationException("Windows not supported");
-        } else if (os.contains("mac")) {
-            return "http://docker.for.mac.host.internal";
-        } else {
-            return "localhost";
-        }
-    }
+    clear();
+    container.shutdown();
+  }
 
-    @SuppressWarnings("unchecked")
-    private void send(String msg, String topic) {
-        SubscriberBuilder<? extends Message<?>, Void> subscriber = createSinkSubscriber(topic);
-        Flowable.fromArray(msg)
-                .map(Message::of)
-                .safeSubscribe((Subscriber<Message<String>>) subscriber.build());
+  @Test
+  public void testSourceAndSink() {
+    if (!isTestSupported()) {
+      System.out.println("Test ignored on this operating system");
+      return;
     }
+    ConsumptionBean bean = container.select(ConsumptionBean.class).get();
+    send("Hello-0", topic);
+    await().until(() -> bean.messages().size() == 1);
+    assertThat(bean.messages().get(0)).isEqualTo("Hello-0");
+    for (int i = 1; i < 11; i++) {
+      send("Hello-" + i, topic);
+    }
+    await().until(() -> bean.messages().size() == 11);
+    assertThat(bean.messages()).allSatisfy(s -> assertThat(s).startsWith("Hello"));
+  }
 
-    private SubscriberBuilder<? extends Message<?>, Void> createSinkSubscriber(String topic) {
-        Map<String, Object> config = new HashMap<>();
-        config.put("connector", SnsConnector.CONNECTOR_NAME);
-        config.put("channel", topic.concat("-transformed"));
-        config.put("sns-url", String.format("http://%s:%d", ip(), port()));
-        SnsConnector snsConnector = new SnsConnector();
-        snsConnector.initConnector();
-        return snsConnector.getSubscriberBuilder(new MapBasedConfig(config));
+  private MapBasedConfig createSourceConfig(String topic) {
+    String prefix = "mp.messaging.incoming.source.";
+    Map<String, Object> config = new HashMap<>();
+    config.put(prefix.concat("connector"), SnsConnector.CONNECTOR_NAME);
+    config.put(prefix.concat("channel"), topic.concat("-transformed"));
+    String appUrl = getAppUrl();
+    int port = 8389;
+    config.put(prefix.concat("port"), port);
+    config.put("sns-app-host", appUrl);
+    config.put("mock-sns-topics", true);
+    config.put("sns-url", String.format("http://%s:%d", ip(), port()));
+    return new MapBasedConfig(config);
+  }
+
+  private boolean isTestSupported() {
+    String os = System.getProperty("os.name").toLowerCase();
+    return os.contains("mac");
+  }
+
+  private String getAppUrl() {
+    String os = System.getProperty("os.name").toLowerCase();
+    if (os.contains("win")) {
+      throw new UnsupportedOperationException("Windows not supported");
+    } else if (os.contains("mac")) {
+      return "http://docker.for.mac.host.internal";
+    } else {
+      return "localhost";
     }
+  }
+
+  @SuppressWarnings("unchecked")
+  private void send(String msg, String topic) {
+    SubscriberBuilder<? extends Message<?>, Void> subscriber = createSinkSubscriber(topic);
+    Flowable.fromArray(msg)
+      .map(Message::of)
+      .safeSubscribe((Subscriber<Message<String>>) subscriber.build());
+  }
+
+  private SubscriberBuilder<? extends Message<?>, Void> createSinkSubscriber(String topic) {
+    Map<String, Object> config = new HashMap<>();
+    config.put("connector", SnsConnector.CONNECTOR_NAME);
+    config.put("channel", topic.concat("-transformed"));
+    config.put("sns-url", String.format("http://%s:%d", ip(), port()));
+    SnsConnector snsConnector = new SnsConnector();
+    snsConnector.initConnector();
+    return snsConnector.getSubscriberBuilder(new MapBasedConfig(config));
+  }
 }
