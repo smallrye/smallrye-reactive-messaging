b9a113cd9654845141085d0025497353bea9e2b3
Georgios Andrianakis <geoand@gmail.com>
2019-09-30 08:45:48 +0300

    Add target to annotations
    This is done to make it easier to use them in Kotlin
    
    Fixes: #209

--- smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/annotations/Broadcast.java
+++ smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/annotations/Broadcast.java
@@ -1,7 +1,13 @@
 package io.smallrye.reactive.messaging.annotations;
 
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Configure if the annotated publisher should dispatch the messages to several subscribers.
@@ -10,11 +16,12 @@ import java.lang.annotation.RetentionPolicy;
  *
  */
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ METHOD, CONSTRUCTOR, FIELD, PARAMETER })
 public @interface Broadcast {
 
     /**
      * Indicates the number of subscribers required before dispatching the items.
-     * 
+     *
      * @return the value, 0 indicates immediate.
      */
     int value() default 0;
