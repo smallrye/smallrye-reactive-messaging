334c7842f7e1ec812475c9db8a945f9707c136b7
Georgios Andrianakis <geoand@gmail.com>
2019-09-30 09:17:17 +0300

    Add Kotlin quickstart example

---
+++ examples/kafka-quickstart-kotlin/src/main/kotlin/acme/Receiver.kt
@@ -0,0 +1,22 @@
+package acme
+
+import io.smallrye.reactive.messaging.kafka.KafkaMessage
+import org.eclipse.microprofile.reactive.messaging.Incoming
+import java.util.concurrent.CompletionStage
+import javax.enterprise.context.ApplicationScoped
+
+@ApplicationScoped
+open class Receiver {
+
+    @Incoming("kafka")
+    open fun consume(message: KafkaMessage<String, String>): CompletionStage<Void> {
+        val payload = message.payload
+        val key = message.key
+        val headers = message.headers
+        val partition = message.partition
+        val timestamp = message.timestamp
+        println("received: " + payload + " from topic " + message.topic)
+        return message.ack()
+    }
+
+}
