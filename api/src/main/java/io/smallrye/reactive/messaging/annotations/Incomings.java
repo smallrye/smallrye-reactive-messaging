22b015655b27b57fc4e681b56b2dff83f43ce418
Clement Escoffier <clement.escoffier@gmail.com>
2019-11-27 20:45:20 +0100

    Add documentation in @Incomings

--- api/src/main/java/io/smallrye/reactive/messaging/annotations/Incomings.java
+++ api/src/main/java/io/smallrye/reactive/messaging/annotations/Incomings.java
@@ -8,13 +8,37 @@ import java.lang.annotation.Target;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 /**
- * Container annotation to declare several {@link Incoming}.
+ * This class is used to allow multiple {@link Incoming} declarations.
+ * You can either use:
+ *
+ * <pre><code>
+ * &#64;Incoming("a")
+ * &#64;Incoming("b")
+ * public void consume(T t);
+ * </code></pre>
+ *
+ * Or use the {@link Incomings} annotation as container:
+ *
+ * <pre><code>
+ * &#64;Incomings({
+ *    &#64;Incoming("a")
+ *    &#64;Incoming("b")
+ * })
+ * public void consume(T t);
+ * </code>
+ *
+ * </pre>
+ *
  * <strong>NOTE:</strong> <em>Experimental</em>, not part of the specification.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Incomings {
 
+    /**
+     * @return the array of {@link Incoming}, must not contain {@code null}. All the included {@link Incoming} must be
+     * value (have a non-blank and non-null channel name).
+     */
     Incoming[] value();
 
 }
