1b0cef6679a008feb22fcb59e1d5d6cc3c1d1647
Clement Escoffier <clement.escoffier@gmail.com>
2020-01-29 15:44:33 +0100

    reosrer imports

--- examples/gcp-pubsub-quickstart/src/main/java/gcp.pubsub/BeanUsingAnEmitter.java
+++ examples/gcp-pubsub-quickstart/src/main/java/gcp.pubsub/BeanUsingAnEmitter.java
@@ -1,13 +1,14 @@
 package gcp.pubsub;
 
-import io.smallrye.reactive.messaging.Emitter;
-import io.smallrye.reactive.messaging.annotations.Channel;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import io.smallrye.reactive.messaging.Emitter;
+import io.smallrye.reactive.messaging.annotations.Channel;
 
 @ApplicationScoped
 public class BeanUsingAnEmitter {
@@ -19,12 +20,12 @@ public class BeanUsingAnEmitter {
     public void periodicallySendMessage() {
         final AtomicInteger counter = new AtomicInteger();
         Executors.newSingleThreadScheduledExecutor()
-            .scheduleAtFixedRate(() -> {
+                .scheduleAtFixedRate(() -> {
                     String message = "Hello " + counter.getAndIncrement();
                     System.out.println("Emitting: " + message);
                     emitter.send(message);
                 },
-                5, 10, TimeUnit.SECONDS);
+                        5, 10, TimeUnit.SECONDS);
     }
 
 }
--- examples/gcp-pubsub-quickstart/src/main/java/gcp.pubsub/Receiver.java
+++ examples/gcp-pubsub-quickstart/src/main/java/gcp.pubsub/Receiver.java
@@ -1,10 +1,12 @@
 package gcp.pubsub;
 
-import io.smallrye.reactive.messaging.gcp.pubsub.PubSubMessage;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
+import java.util.concurrent.CompletionStage;
 
 import javax.enterprise.context.ApplicationScoped;
-import java.util.concurrent.CompletionStage;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import io.smallrye.reactive.messaging.gcp.pubsub.PubSubMessage;
 
 @ApplicationScoped
 public class Receiver {
