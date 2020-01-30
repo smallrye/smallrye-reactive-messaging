c25feb616300e70defad4621922c5e766dd27e54
Gunnar Morling <gunnar.morling@googlemail.com>
2020-01-30 16:45:23 +0100

    #388 JavaDoc adjustments

--- smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/KafkaRecord.java
+++ smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/KafkaRecord.java
@@ -7,13 +7,13 @@ import org.eclipse.microprofile.reactive.messaging.Message;
 public interface KafkaRecord<K, T> extends Message<T> {
 
     /**
-     * Creates a new outgoing kafka message.
+     * Creates a new outgoing Kafka record.
      *
      * @param key the key, can be {@code null}
      * @param value the value / payload, must not be {@code null}
      * @param <K> the type of the key
      * @param <T> the type of the value
-     * @return the new outgoing kafka message
+     * @return the new outgoing Kafka record
      */
     static <K, T> OutgoingKafkaRecord<K, T> of(K key, T value) {
         return new OutgoingKafkaRecord<>(null, key, value, -1, -1,
@@ -21,21 +21,21 @@ public interface KafkaRecord<K, T> extends Message<T> {
     }
 
     /**
-     * Creates a new outgoing kafka message.
+     * Creates a new outgoing Kafka record.
      *
      * @param topic the topic, must not be {@code null}
      * @param key the key, can be {@code null}
      * @param value the value / payload, must not be {@code null}
      * @param <K> the type of the key
      * @param <T> the type of the value
-     * @return the new outgoing kafka message
+     * @return the new outgoing Kafka record
      */
     static <K, T> OutgoingKafkaRecord<K, T> of(String topic, K key, T value) {
         return new OutgoingKafkaRecord<>(topic, key, value, -1, -1, new RecordHeaders(), null);
     }
 
     /**
-     * Creates a new outgoing kafka message.
+     * Creates a new outgoing Kafka record.
      *
      * @param topic the topic, must not be {@code null}
      * @param key the key, can be {@code null}
@@ -44,7 +44,7 @@ public interface KafkaRecord<K, T> extends Message<T> {
      * @param partition the partition, can be {@code -1} to indicate no partition
      * @param <K> the type of the key
      * @param <T> the type of the value
-     * @return the new outgoing kafka message
+     * @return the new outgoing Kafka record
      */
     static <K, T> OutgoingKafkaRecord<K, T> of(String topic, K key, T value, long timestamp, int partition) {
         return new OutgoingKafkaRecord<>(topic, key, value, timestamp, partition, new RecordHeaders(), null);
