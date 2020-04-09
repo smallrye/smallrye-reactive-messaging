d883f4a16c88e96c619728d0a2f88bae2e5ec43c
Rafael de Andrade Sousa <rafael.and@gmail.com>
2020-04-09 10:35:31 -0400

    Not using these files for now

--- examples/pulsar-quickstart/src/main/java/acme/Main.java
+++ examples/pulsar-quickstart/src/main/java/acme/Main.java
@@ -1,15 +1,10 @@
 package acme;
 
-import io.smallrye.reactive.messaging.pulsar.PulsarMessage;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-import org.eclipse.microprofile.reactive.streams.operators.PublisherBuilder;
-import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
 
