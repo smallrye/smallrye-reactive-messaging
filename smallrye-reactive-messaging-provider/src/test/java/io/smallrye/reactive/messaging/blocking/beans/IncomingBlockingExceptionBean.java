4567a87953f2399f70dded242c1275b9a495f1ba
Ken Finnigan <ken@kenfinnigan.me>
2020-07-02 14:32:31 -0400

    Fixes #629
    - @Blocking and @Incoming method exceptions are now logged
    - JUnit 4/5 co-existing now for unit tests
    - Add SmallRye Testing to capture log output from a test

---
+++ smallrye-reactive-messaging-provider/src/test/java/io/smallrye/reactive/messaging/blocking/beans/IncomingBlockingExceptionBean.java
@@ -0,0 +1,28 @@
+package io.smallrye.reactive.messaging.blocking.beans;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import io.smallrye.reactive.messaging.annotations.Blocking;
+
+@ApplicationScoped
+public class IncomingBlockingExceptionBean {
+    private final List<String> list = new CopyOnWriteArrayList<>();
+
+    @Incoming("in")
+    @Blocking
+    public void consume(String s) {
+        if (s.equals("c")) {
+            throw new RuntimeException("KABOOM");
+        }
+        list.add(s);
+    }
+
+    public List<String> list() {
+        return list;
+    }
+}
