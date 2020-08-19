41b4b05ddeaf09b75fbc915c4b9d0a207bcff01a
Clement Escoffier <clement.escoffier@gmail.com>
2020-08-19 10:02:14 +0200

    Fix #673
    Add two new nack strategies to the AMQP connector:
    
    * modified-failed - marking the message modified and incrementing the delivery count (the message may be redelivered to the application)
    * modified-failed-undeliverable-here - marking the message modified, incrementing the delivery count, and indicating the application cannot process the message. The broker cannot redeliver the message to the application.

--- smallrye-reactive-messaging-amqp/src/test/java/io/smallrye/reactive/messaging/amqp/AmqpFailureHandlerTest.java
+++ smallrye-reactive-messaging-amqp/src/test/java/io/smallrye/reactive/messaging/amqp/AmqpFailureHandlerTest.java
@@ -45,8 +45,17 @@ public class AmqpFailureHandlerTest extends AmqpTestBase {
         return container.getBeanManager().createInstance().select(MyReceiverBean.class).get();
     }
 
+    private MyReceiverBeanRecovering deployRecovering() {
+        Weld weld = new Weld();
+        weld.addBeanClass(MyReceiverBeanRecovering.class);
+
+        container = weld.initialize();
+        await().until(() -> container.select(MediatorManager.class).get().isInitialized());
+        return container.getBeanManager().createInstance().select(MyReceiverBeanRecovering.class).get();
+    }
+
     @Test
-    public void testFailStrategy() throws InterruptedException {
+    public void testFailStrategy() {
         String address = UUID.randomUUID().toString();
         getFailConfig(address);
         MyReceiverBean bean = deploy();
@@ -131,6 +140,46 @@ public class AmqpFailureHandlerTest extends AmqpTestBase {
         assertThat(isAmqpConnectorReady(connector)).isTrue();
     }
 
+    @Test
+    public void testModifiedFailedStrategy() {
+        getModifiedFailedConfig();
+        MyReceiverBeanRecovering bean = deployRecovering();
+        AtomicInteger counter = new AtomicInteger();
+        AmqpConnector connector = container.getBeanManager().createInstance().select(AmqpConnector.class,
+                ConnectorLiteral.of(AmqpConnector.CONNECTOR_NAME)).get();
+        await().until(() -> isAmqpConnectorReady(connector));
+        await().until(() -> isAmqpConnectorAlive(connector));
+
+        usage.produceTenIntegers("modified-failed", counter::getAndIncrement);
+
+        await().atMost(2, TimeUnit.MINUTES).until(() -> bean.list().size() >= 13);
+        // All messages should have been received + 3, 6 and 9 are redelivered
+        assertThat(bean.list()).containsExactlyInAnyOrder(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 6, 9);
+
+        assertThat(isAmqpConnectorAlive(connector)).isTrue();
+        assertThat(isAmqpConnectorReady(connector)).isTrue();
+    }
+
+    @Test
+    public void testModifiedFailedUndeliverableHereStrategy() {
+        getModifiedFailedUndeliverableConfig();
+        MyReceiverBeanRecovering bean = deployRecovering();
+        AtomicInteger counter = new AtomicInteger();
+        AmqpConnector connector = container.getBeanManager().createInstance().select(AmqpConnector.class,
+                ConnectorLiteral.of(AmqpConnector.CONNECTOR_NAME)).get();
+        await().until(() -> isAmqpConnectorReady(connector));
+        await().until(() -> isAmqpConnectorAlive(connector));
+
+        usage.produceTenIntegers("modified-failed-undeliverable-here", counter::getAndIncrement);
+
+        await().atMost(2, TimeUnit.MINUTES).until(() -> bean.list().size() >= 10);
+        // All messages should have been received, 3 6 and 9 are NOT redelivered
+        assertThat(bean.list()).containsExactly(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+        assertThat(isAmqpConnectorAlive(connector)).isTrue();
+        assertThat(isAmqpConnectorReady(connector)).isTrue();
+    }
+
     private void getFailConfig(String address) {
         new MapBasedConfig()
                 .put("mp.messaging.incoming.amqp.address", address)
@@ -183,15 +232,63 @@ public class AmqpFailureHandlerTest extends AmqpTestBase {
                 .write();
     }
 
+    private void getModifiedFailedConfig() {
+        new MapBasedConfig()
+                .put("mp.messaging.incoming.amqp.address", "modified-failed")
+                .put("mp.messaging.incoming.amqp.connector", AmqpConnector.CONNECTOR_NAME)
+                .put("mp.messaging.incoming.amqp.host", host)
+                .put("mp.messaging.incoming.amqp.port", port)
+                .put("mp.messaging.incoming.amqp.durable", true)
+                .put("amqp-username", username)
+                .put("amqp-password", password)
+                .put("mp.messaging.incoming.amqp.failure-strategy", "modified-failed")
+                .write();
+    }
+
+    private void getModifiedFailedUndeliverableConfig() {
+        new MapBasedConfig()
+                .put("mp.messaging.incoming.amqp.address", "modified-failed-undeliverable-here")
+                .put("mp.messaging.incoming.amqp.connector", AmqpConnector.CONNECTOR_NAME)
+                .put("mp.messaging.incoming.amqp.host", host)
+                .put("mp.messaging.incoming.amqp.port", port)
+                .put("mp.messaging.incoming.amqp.durable", true)
+                .put("amqp-username", username)
+                .put("amqp-password", password)
+                .put("mp.messaging.incoming.amqp.failure-strategy", "modified-failed-undeliverable-here")
+                .write();
+    }
+
     @ApplicationScoped
     public static class MyReceiverBean {
-        private List<Integer> received = new CopyOnWriteArrayList<>();
+        private final List<Integer> received = new CopyOnWriteArrayList<>();
 
         @Incoming("amqp")
         public CompletionStage<Void> process(AmqpMessage<Integer> record) {
             Integer payload = record.getPayload();
             received.add(payload);
             if (payload != 0 && payload % 3 == 0) {
+                return record.nack(new IllegalArgumentException("nack 3 - " + payload));
+            }
+            return record.ack();
+        }
+
+        public List<Integer> list() {
+            return received;
+        }
+
+    }
+
+    @ApplicationScoped
+    public static class MyReceiverBeanRecovering {
+        private final List<Integer> received = new CopyOnWriteArrayList<>();
+        private final List<Integer> failed = new CopyOnWriteArrayList<>();
+
+        @Incoming("amqp")
+        public CompletionStage<Void> process(AmqpMessage<Integer> record) {
+            Integer payload = record.getPayload();
+            received.add(payload);
+            if (payload != 0 && payload % 3 == 0 && !failed.contains(payload)) {
+                failed.add(payload);
                 return record.nack(new IllegalArgumentException("nack 3 - " + payload));
             }
             return record.ack();
