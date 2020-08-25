5633563dbb22698e6b8a9457655f64f659a26d71
Clement Escoffier <clement.escoffier@gmail.com>
2020-08-25 12:27:01 +0200

    Disable retry in KafkaSinkTest

--- smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/KafkaTestBase.java
+++ smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/KafkaTestBase.java
@@ -41,9 +41,9 @@ public class KafkaTestBase {
         File directory = Testing.Files.createTestingDirectory(System.getProperty("java.io.tmpdir"), true);
         kafka = new KafkaCluster().withPorts(2182, 9092).addBrokers(1)
                 .usingDirectory(directory)
+                .deleteDataPriorToStartup(true)
                 .deleteDataUponShutdown(false)
                 .withKafkaConfiguration(props)
-                .deleteDataPriorToStartup(true)
                 .startup();
     }
 
