ac09b7fe71290c03c08188b0799476c8b0da3edc
Rafael de Andrade Sousa <rafael.and@gmail.com>
2021-02-26 20:26:25 -0500

    Updating the package names

--- examples/pulsar-quickstart/src/main/java/acme/Main.java
+++ examples/pulsar-quickstart/src/main/java/acme/Main.java
@@ -1,10 +1,8 @@
 package acme;
 
-/*
-TODO : https://github.com/rafaelsousa/smallrye-reactive-messaging/issues/15
- */
-//import org.eclipse.microprofile.reactive.messaging.Incoming;
-//import org.eclipse.microprofile.reactive.messaging.Outgoing;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
 
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
@@ -16,13 +14,13 @@ public class Main {
         SeContainer container = SeContainerInitializer.newInstance().initialize();
     }
 
-//    @Incoming("producer")
+    @Incoming("producer")
     public void consume(byte [] message) {
         System.out.println("received: " + String.valueOf(message));
     }
 
 
-//    @Outgoing("consumer")
+    @Outgoing("consumer")
     public String send() {
         return "Message";
     }
