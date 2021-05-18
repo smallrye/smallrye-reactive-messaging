36acaacf5162ece98d6ef1ad9b161325e19be61e
Clement Escoffier <clement.escoffier@gmail.com>
2021-05-18 13:56:08 +0200

    [RELEASE] - Next development version: 3.4.0-SNAPSHOT

--- smallrye-reactive-messaging-provider/src/test/java/io/smallrye/reactive/messaging/impl/ConnectorConfigTest.java
+++ smallrye-reactive-messaging-provider/src/test/java/io/smallrye/reactive/messaging/impl/ConnectorConfigTest.java
@@ -95,7 +95,8 @@ public class ConnectorConfigTest {
 
         Iterable<String> names = config.getPropertyNames();
         assertThat(names)
-                .containsExactlyInAnyOrder("connector", "ATTR1", "attr1", "attr2", "attr.2", "ATTR", "attr", "AT_TR", "at.tr", "at-tr", "key",
+                .containsExactlyInAnyOrder("connector", "ATTR1", "attr1", "attr2", "attr.2", "ATTR", "attr", "AT_TR", "at.tr",
+                        "at-tr", "key",
                         "SOME_KEY", "some-key", "SOME_OTHER_KEY", "ATTR3", "attr4", "channel-name");
 
         assertThat(config.getOptionalValue("connector", String.class)).hasValue("some-connector");
