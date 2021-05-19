64574030e49a0fa083b853e72a4043061e1ad0d3
Roberto Cortez <radcortez@yahoo.com>
2021-05-19 15:44:03 +0100

    Make OpenGraphException serializable

--- smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/wiring/WiringException.java
+++ smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/wiring/WiringException.java
@@ -3,4 +3,10 @@ package io.smallrye.reactive.messaging.wiring;
 import javax.enterprise.inject.UnsatisfiedResolutionException;
 
 public class WiringException extends UnsatisfiedResolutionException {
+    public WiringException() {
+    }
+
+    public WiringException(final String message) {
+        super(message);
+    }
 }
