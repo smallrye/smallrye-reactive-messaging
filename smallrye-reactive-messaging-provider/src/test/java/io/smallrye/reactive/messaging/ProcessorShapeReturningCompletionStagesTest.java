d1a05a9336f19b95e75949097f889917b8e93fe9
Ozan Gunalp <ozangunalp@gmail.com>
2021-06-17 19:41:36 +0100

    Reproducer: returning completion stage from processors required matching Item or Message from parameter and returning type

--- smallrye-reactive-messaging-provider/src/test/java/io/smallrye/reactive/messaging/ProcessorShapeReturningCompletionStagesTest.java
+++ smallrye-reactive-messaging-provider/src/test/java/io/smallrye/reactive/messaging/ProcessorShapeReturningCompletionStagesTest.java
@@ -8,6 +8,8 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import io.smallrye.mutiny.Multi;
+import io.smallrye.reactive.messaging.beans.BeanConsumingItemAndProducingCompletionStageOfMessage;
+import io.smallrye.reactive.messaging.beans.BeanConsumingMessagesAndProducingCompletionStageOfSomething;
 import io.smallrye.reactive.messaging.beans.BeanProducingACompletableFuture;
 import io.smallrye.reactive.messaging.beans.BeanProducingACompletableFutureOfMessage;
 import io.smallrye.reactive.messaging.beans.BeanProducingACompletionStage;
@@ -29,8 +31,26 @@ public class ProcessorShapeReturningCompletionStagesTest extends WeldTestBase {
     }
 
     @Test
+    public void testBeanConsumingItemAndProducingACompletionStageOfMessage() {
+        addBeanClass(BeanConsumingItemAndProducingCompletionStageOfMessage.class);
+        initialize();
+        MyCollector collector = container.select(MyCollector.class).get();
+        await().until(() -> collector.payloads().size() == LIST.size());
+        assertThat(collector.payloads()).isEqualTo(LIST);
+    }
+
+    @Test
     public void testBeanProducingACompletionStageOfPayloads() {
         addBeanClass(BeanProducingACompletionStage.class);
+        initialize();
+        MyCollector collector = container.select(MyCollector.class).get();
+        await().until(() -> collector.payloads().size() == LIST.size());
+        assertThat(collector.payloads()).isEqualTo(LIST);
+    }
+
+    @Test
+    public void testBeanConsumingMessageAndProducingACompletionStageOfPayloads() {
+        addBeanClass(BeanConsumingMessagesAndProducingCompletionStageOfSomething.class);
         initialize();
         MyCollector collector = container.select(MyCollector.class).get();
         await().until(() -> collector.payloads().size() == LIST.size());
