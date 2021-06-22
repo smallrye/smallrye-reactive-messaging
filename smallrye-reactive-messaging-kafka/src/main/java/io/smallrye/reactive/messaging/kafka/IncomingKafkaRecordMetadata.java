900207e7da1bf4c7a55ec15923faa0b2b9031e4b
Kabir Khan <kkhan@redhat.com>
2021-06-22 15:07:53 +0100

    Smaller API surface for KafkaMessageMetadata and related interfaces
    The intent is:
    * to not pull in in progress spec constructs (such as Metadata)
    for projects which need strict spec compliance.
    * to make it clearer what is API, and what are implementation details (this is a start,
     not the end)

--- smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/IncomingKafkaRecordMetadata.java
+++ smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/IncomingKafkaRecordMetadata.java
@@ -1,72 +1,16 @@
 package io.smallrye.reactive.messaging.kafka;
 
-import java.time.Instant;
-
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.common.header.Header;
-import org.apache.kafka.common.header.Headers;
-import org.apache.kafka.common.header.internals.RecordHeader;
-import org.apache.kafka.common.header.internals.RecordHeaders;
-import org.apache.kafka.common.record.TimestampType;
 
-public class IncomingKafkaRecordMetadata<K, T> implements KafkaMessageMetadata<K> {
-
-    private final K recordKey;
-    private final String topic;
-    private final int partition;
-    private final TimestampType timestampType;
-    private final long offset;
-    private final ConsumerRecord<K, T> record;
+/**
+ * @deprecated use {@link io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata} instead
+ */
+@Deprecated
+public class IncomingKafkaRecordMetadata<K, T>
+        extends io.smallrye.reactive.messaging.kafka.api.IncomingKafkaRecordMetadata<K, T> implements KafkaMessageMetadata<K> {
 
     public IncomingKafkaRecordMetadata(ConsumerRecord<K, T> record) {
-        this.record = record;
-        this.recordKey = record.key();
-        this.topic = record.topic();
-        this.partition = record.partition();
-        this.timestampType = record.timestampType();
-        this.offset = record.offset();
+        super(record);
     }
 
-    @Override
-    public String getTopic() {
-        return topic;
-    }
-
-    @Override
-    public K getKey() {
-        return recordKey;
-    }
-
-    @Override
-    public int getPartition() {
-        return partition;
-    }
-
-    @Override
-    public Instant getTimestamp() {
-        return Instant.ofEpochMilli(record.timestamp());
-    }
-
-    public TimestampType getTimestampType() {
-        return timestampType;
-    }
-
-    public long getOffset() {
-        return offset;
-    }
-
-    @Override
-    public Headers getHeaders() {
-        Headers headers = new RecordHeaders();
-        if (record.headers() != null) {
-            for (Header header : record.headers()) {
-                headers.add(new RecordHeader(header.key(), header.value()));
-            }
-        }
-        return headers;
-    }
-
-    public ConsumerRecord<K, T> getRecord() {
-        return record;
-    }
 }
--- smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/OutgoingKafkaRecordMetadata.java
+++ smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/OutgoingKafkaRecordMetadata.java
@@ -1,21 +1,17 @@
 package io.smallrye.reactive.messaging.kafka;
 
 import java.time.Instant;
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.kafka.common.header.Header;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.header.internals.RecordHeader;
-import org.apache.kafka.common.header.internals.RecordHeaders;
 
-public class OutgoingKafkaRecordMetadata<K> implements KafkaMessageMetadata<K> {
-
-    private final String topic;
-    private final K recordKey;
-    private final int partition;
-    private final Instant timestamp;
-    private final Headers headers;
+/**
+ * @deprecated use {@link io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata} instead
+ */
+@Deprecated
+public class OutgoingKafkaRecordMetadata<K> extends io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata<K>
+        implements KafkaMessageMetadata<K> {
 
     public static <K> OutgoingKafkaRecordMetadataBuilder<K> builder() {
         return new OutgoingKafkaRecordMetadataBuilder<>();
@@ -23,77 +19,51 @@ public class OutgoingKafkaRecordMetadata<K> implements KafkaMessageMetadata<K> {
 
     public OutgoingKafkaRecordMetadata(String topic, K key, int partition, Instant timestamp,
             Headers headers) {
-        this.topic = topic;
-        this.recordKey = key;
-        this.partition = partition;
-        this.timestamp = timestamp;
-        this.headers = headers;
+        super(topic, key, partition, timestamp, headers);
     }
 
-    @Override
-    public int getPartition() {
-        return partition;
-    }
+    public static final class OutgoingKafkaRecordMetadataBuilder<K>
+            extends io.smallrye.reactive.messaging.kafka.api.OutgoingKafkaRecordMetadata.OutgoingKafkaRecordMetadataBuilder<K> {
 
-    @Override
-    public Instant getTimestamp() {
-        return timestamp;
-    }
-
-    @Override
-    public Headers getHeaders() {
-        return headers;
-    }
-
-    @Override
-    public String getTopic() {
-        return topic;
-    }
-
-    @Override
-    public K getKey() {
-        return recordKey;
-    }
-
-    public static final class OutgoingKafkaRecordMetadataBuilder<K> {
-        private String topic;
-        private K recordKey;
-        private int partition = -1;
-        private Instant timestamp = null;
-        private Headers headers;
-
+        @Override
         public OutgoingKafkaRecordMetadataBuilder<K> withTopic(String topic) {
-            this.topic = topic;
+            super.withTopic(topic);
             return this;
         }
 
+        @Override
         public OutgoingKafkaRecordMetadataBuilder<K> withKey(K recordKey) {
-            this.recordKey = recordKey;
+            super.withKey(recordKey);
             return this;
         }
 
+        @Override
         public OutgoingKafkaRecordMetadataBuilder<K> withPartition(int partition) {
-            this.partition = partition;
+            super.withPartition(partition);
             return this;
         }
 
+        @Override
         public OutgoingKafkaRecordMetadataBuilder<K> withTimestamp(Instant timestamp) {
-            this.timestamp = timestamp;
+            super.withTimestamp(timestamp);
             return this;
         }
 
+        @Override
         public OutgoingKafkaRecordMetadataBuilder<K> withHeaders(Headers headers) {
-            this.headers = headers;
+            super.withHeaders(headers);
+            return this;
+        }
+
+        @Override
+        public OutgoingKafkaRecordMetadataBuilder<K> withHeaders(List<RecordHeader> headers) {
+            super.withHeaders(headers);
             return this;
         }
 
         public OutgoingKafkaRecordMetadata<K> build() {
-            return new OutgoingKafkaRecordMetadata<>(topic, recordKey, partition, timestamp, headers);
-        }
-
-        public OutgoingKafkaRecordMetadataBuilder<K> withHeaders(List<RecordHeader> headers) {
-            List<Header> iterable = new ArrayList<>(headers);
-            return withHeaders(new RecordHeaders(iterable));
+            return new OutgoingKafkaRecordMetadata<>(
+                    getTopic(), getRecordKey(), getPartition(), getTimestamp(), getHeaders());
         }
     }
 }
