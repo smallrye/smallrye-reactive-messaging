102646f3c0d5b0684fbb810045eb5ca74150381b
Charlie Dennison <charles.dennison@verizon.com>
2022-04-07 11:22:22 -0400

    adding rabbitmq quickstart

---
+++ examples/rabbitmq-quickstart/src/main/java/acme/BeanUsingAnEmitter.java
@@ -0,0 +1,28 @@
+package acme;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+
+@ApplicationScoped
+public class BeanUsingAnEmitter {
+
+    // Not sure why this doesn't work - plan to raise a bug
+    //    @Inject
+    //    @Channel("to-rabbitmq-string")
+    Emitter<String> emitter;
+
+    public void periodicallySendMessage() {
+        AtomicInteger counter = new AtomicInteger();
+        Executors.newSingleThreadScheduledExecutor()
+                .scheduleAtFixedRate(() -> {
+                    emitter.send("Price from emitter " + counter.getAndIncrement());
+                },
+                        1, 1, TimeUnit.SECONDS);
+    }
+
+}
---
+++ examples/rabbitmq-quickstart/src/main/java/acme/Receiver.java
@@ -0,0 +1,23 @@
+package acme;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+
+import io.vertx.core.json.JsonObject;
+
+@ApplicationScoped
+public class Receiver {
+
+    @Incoming("from-rabbitmq-string")
+    public void consume(String p) {
+        System.out.println("received string: " + p);
+    }
+
+    @Incoming("from-rabbitmq-jsonobject")
+    public void consume(JsonObject p) {
+        Price price = p.mapTo(Price.class);
+        System.out.println("received jsonobject price: " + price.price);
+    }
+
+}
