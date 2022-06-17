0386e8dfb5863febc6a214baa3991e9e50b8ff29
Kevin Wooten <kevin@wooten.com>
2022-06-16 18:21:33 -0700

    Only allow a single ack or nack to be sent to RabbitMQ per message
    Currently if the calls ack or nack more than a combine total of 1 time the client will send all the acks or nacks to the server. Since RabbitMQ explicitly disallows this it causes a connection error which then causes a complete connection restart.  The worste part is that without some real examination you cannot tell this is from multiply acking or nacking.
    
    The best example of this is when a use `nacks` a message in a message handler that uses the `POST_PROCESSING` strategy. The framework will then `ack` or `nack` the message based on the outcome of the message handler.  No logging or hints exist to tell users that this is what is causing connection restarts.
    
    This commit replaces the ack and nack handlers with versions that are no-ops; effectively ignoring any attempt to ack or nack after the first one.
    
    Note that this will probably cause the message stream to fail as well; although it will do so with a relavent error message that a user can use to fix the code.

---
+++ smallrye-reactive-messaging-rabbitmq/src/test/java/io/smallrye/reactive/messaging/rabbitmq/IncomingRabbitMQMessageTest.java
@@ -0,0 +1,73 @@
+package io.smallrye.reactive.messaging.rabbitmq;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
+import org.junit.jupiter.api.Test;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Envelope;
+
+import io.smallrye.reactive.messaging.rabbitmq.ack.RabbitMQAckHandler;
+import io.smallrye.reactive.messaging.rabbitmq.fault.RabbitMQFailureHandler;
+import io.vertx.mutiny.core.Context;
+import io.vertx.mutiny.rabbitmq.RabbitMQMessage;
+
+public class IncomingRabbitMQMessageTest {
+
+    RabbitMQAckHandler doNothingAck = new RabbitMQAckHandler() {
+        @Override
+        public <V> CompletionStage<Void> handle(IncomingRabbitMQMessage<V> message, Context context) {
+            return CompletableFuture.completedFuture(null);
+        }
+    };
+
+    RabbitMQFailureHandler doNothingNack = new RabbitMQFailureHandler() {
+        @Override
+        public <V> CompletionStage<Void> handle(IncomingRabbitMQMessage<V> message, Context context, Throwable reason) {
+            return CompletableFuture.completedFuture(null);
+        }
+    };
+
+    @Test
+    public void testDoubleAckBehavior() {
+
+        RabbitMQMessage msg = RabbitMQMessage.newInstance(mock(io.vertx.rabbitmq.RabbitMQMessage.class));
+        when(msg.properties()).thenReturn(new BasicProperties());
+        when(msg.envelope()).thenReturn(new Envelope(13456, false, "test", "test"));
+
+        Exception nackReason = new Exception("test");
+
+        IncomingRabbitMQMessage<String> ackMsg = new IncomingRabbitMQMessage<>(msg, mock(ConnectionHolder.class), false,
+                doNothingNack,
+                doNothingAck,
+                "text/plain");
+
+        assertDoesNotThrow(() -> ackMsg.ack().toCompletableFuture().get());
+        assertDoesNotThrow(() -> ackMsg.ack().toCompletableFuture().get());
+        assertDoesNotThrow(() -> ackMsg.nack(nackReason).toCompletableFuture().get());
+    }
+
+    @Test
+    public void testDoubleNackBehavior() {
+
+        RabbitMQMessage msg = RabbitMQMessage.newInstance(mock(io.vertx.rabbitmq.RabbitMQMessage.class));
+        when(msg.properties()).thenReturn(new BasicProperties());
+        when(msg.envelope()).thenReturn(new Envelope(13456, false, "test", "test"));
+
+        Exception nackReason = new Exception("test");
+
+        IncomingRabbitMQMessage<String> nackMsg = new IncomingRabbitMQMessage<>(msg, mock(ConnectionHolder.class), false,
+                doNothingNack,
+                doNothingAck,
+                "text/plain");
+
+        assertDoesNotThrow(() -> nackMsg.nack(nackReason).toCompletableFuture().get());
+        assertDoesNotThrow(() -> nackMsg.nack(nackReason).toCompletableFuture().get());
+        assertDoesNotThrow(() -> nackMsg.ack().toCompletableFuture().get());
+    }
+}
