97a602e69dc4a9c8705a1c7f5f94f210d3569548
Ozan Gunalp <ozangunalp@gmail.com>
2022-10-26 10:59:34 +0100

    KafkaCommitHandlerTest Increase unprocessed max age from 1 to 100

--- smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/commit/KafkaCommitHandlerTest.java
+++ smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/commit/KafkaCommitHandlerTest.java
@@ -236,14 +236,14 @@ public class KafkaCommitHandlerTest extends KafkaCompanionTestBase {
                 .with("group.id", "test-source-with-throttled-latest-processed-commit")
                 .with("value.deserializer", IntegerDeserializer.class.getName())
                 .with("commit-strategy", "throttled")
-                .with("throttled.unprocessed-record-max-age.ms", 1);
+                .with("throttled.unprocessed-record-max-age.ms", 100);
 
         MapBasedConfig config2 = newCommonConfigForSource()
                 .with("client.id", UUID.randomUUID().toString())
                 .with("group.id", "test-source-with-throttled-latest-processed-commit")
                 .with("value.deserializer", IntegerDeserializer.class.getName())
                 .with("commit-strategy", "throttled")
-                .with("throttled.unprocessed-record-max-age.ms", 1);
+                .with("throttled.unprocessed-record-max-age.ms", 100);
 
         KafkaConnectorIncomingConfiguration ic1 = new KafkaConnectorIncomingConfiguration(config1);
         KafkaConnectorIncomingConfiguration ic2 = new KafkaConnectorIncomingConfiguration(config2);
