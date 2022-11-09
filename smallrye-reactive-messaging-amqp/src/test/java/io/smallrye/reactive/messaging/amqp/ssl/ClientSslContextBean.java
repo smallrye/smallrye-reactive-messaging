497ce03f6e06241629379c21d9de2d5edc621d0f
Ozan Gunalp <ozangunalp@gmail.com>
2022-11-09 15:28:50 +0000

    [RELEASE] - Next development version: 4.2.0-SNAPSHOT

--- smallrye-reactive-messaging-amqp/src/test/java/io/smallrye/reactive/messaging/amqp/ssl/ClientSslContextBean.java
+++ smallrye-reactive-messaging-amqp/src/test/java/io/smallrye/reactive/messaging/amqp/ssl/ClientSslContextBean.java
@@ -9,10 +9,11 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.KeyStore;
 
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.inject.Produces;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Produces;
 
 import io.smallrye.common.annotation.Identifier;
 
