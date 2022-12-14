3056b9f7eb4affe537c569a5892cc29950c4afec
ozangunalp <ozangunalp@gmail.com>
2022-12-14 15:37:35 +0000

    [RELEASE] - Bump version to 4.1.1

--- smallrye-reactive-messaging-amqp/src/main/java/io/smallrye/reactive/messaging/amqp/AmqpClientHelper.java
+++ smallrye-reactive-messaging-amqp/src/main/java/io/smallrye/reactive/messaging/amqp/AmqpClientHelper.java
@@ -5,9 +5,10 @@ import static io.smallrye.reactive.messaging.amqp.i18n.AMQPLogging.log;
 
 import java.util.Optional;
 
+import javax.net.ssl.SSLContext;
+
 import jakarta.enterprise.inject.Instance;
 import jakarta.enterprise.inject.literal.NamedLiteral;
-import javax.net.ssl.SSLContext;
 
 import io.netty.handler.ssl.ApplicationProtocolConfig;
 import io.netty.handler.ssl.IdentityCipherSuiteFilter;
