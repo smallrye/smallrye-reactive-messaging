b6e1918a002ffce0b11097709e624fc342981386
Ozan Gunalp <ozangunalp@gmail.com>
2023-02-28 14:44:43 +0000

    Weld 5 api changes

--- smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/base/UnsatisfiedInstance.java
+++ smallrye-reactive-messaging-kafka/src/test/java/io/smallrye/reactive/messaging/kafka/base/UnsatisfiedInstance.java
@@ -52,6 +52,16 @@ public class UnsatisfiedInstance<T> implements Instance<T> {
     }
 
     @Override
+    public Handle<T> getHandle() {
+        return null;
+    }
+
+    @Override
+    public Iterable<? extends Handle<T>> handles() {
+        return null;
+    }
+
+    @Override
     public Iterator<T> iterator() {
         return Collections.emptyIterator();
     }
