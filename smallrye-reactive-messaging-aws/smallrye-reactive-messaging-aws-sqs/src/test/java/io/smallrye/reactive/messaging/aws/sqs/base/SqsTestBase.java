a75dd1aeb2979c55856da88ba66e67ef3270898a
holomekc <30546982+holomekc@users.noreply.github.com>
2023-12-05 23:25:54 +0100

    Fix testcontainer issues

--- smallrye-reactive-messaging-aws/smallrye-reactive-messaging-aws-sqs/src/test/java/io/smallrye/reactive/messaging/aws/sqs/base/SqsTestBase.java
+++ smallrye-reactive-messaging-aws/smallrye-reactive-messaging-aws-sqs/src/test/java/io/smallrye/reactive/messaging/aws/sqs/base/SqsTestBase.java
@@ -1,7 +1,10 @@
 package io.smallrye.reactive.messaging.aws.sqs.base;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -11,9 +14,18 @@ import org.jboss.logging.Logger;
 import org.jboss.weld.environment.se.Weld;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.containers.Container;
+import org.testcontainers.containers.ExecInContainerPattern;
 import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
+import org.testcontainers.containers.startupcheck.StartupCheckStrategy;
+import org.testcontainers.containers.wait.strategy.ShellStrategy;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InspectContainerResponse;
 
 import io.smallrye.reactive.messaging.aws.base.JbossLogConsumer;
 import io.smallrye.reactive.messaging.aws.base.WeldTestBase;
@@ -28,14 +40,13 @@ import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
 import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
 import software.amazon.awssdk.services.sqs.model.Message;
 import software.amazon.awssdk.services.sqs.model.PurgeQueueRequest;
+import software.amazon.awssdk.services.sqs.model.QueueAttributeName;
 import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @Testcontainers
 public class SqsTestBase extends WeldTestBase {
 
-    // TODO: Annotation seems to have a racing condition issue with BeforeAll...
-    //  so maybe singleton pattern?
     //@Container
     protected static final LocalStackContainer LOCAL_STACK_CONTAINER =
             new LocalStackContainer(DockerImageName.parse("localstack/localstack:latest"))
@@ -45,21 +56,20 @@ public class SqsTestBase extends WeldTestBase {
                     .withEnv("DISABLE_EVENTS", "1")
                     .withEnv("DNS_ADDRESS", "0")
                     .withEnv("DEBUG", "1")
-                    .withEnv("LS_LOG", "trace");
+                    .withEnv("LS_LOG", "trace")
+                    // Due to latest changes the container is in rdy state too early, which can create connection
+                    // issues. So we wait until it is really rdy.
+                    .waitingFor(new ShellStrategy().withCommand("awslocal sqs list-queues"));
 
     static {
         // https://java.testcontainers.org/test_framework_integration/manual_lifecycle_control/#singleton-containers
         // Ryuk will stop it.
         LOCAL_STACK_CONTAINER.start();
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private static SqsAsyncClient CLIENT;
     protected static final String QUEUE_NAME = "test";
+    protected static final String FIFO_QUEUE_NAME = "test.fifo";
 
     @BeforeAll
     static void baseBeforeAll() throws ExecutionException, InterruptedException {
@@ -71,6 +81,8 @@ public class SqsTestBase extends WeldTestBase {
                 .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
                 .build();
         CLIENT.createQueue(CreateQueueRequest.builder().queueName(QUEUE_NAME).build()).get();
+        CLIENT.createQueue(CreateQueueRequest.builder().queueName(FIFO_QUEUE_NAME)
+                .attributes(Map.of(QueueAttributeName.FIFO_QUEUE, "true")).build()).get();
     }
 
     @Override
