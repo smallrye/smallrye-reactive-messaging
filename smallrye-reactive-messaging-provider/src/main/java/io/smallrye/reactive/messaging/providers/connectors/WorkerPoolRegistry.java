3d32c381d1aba251d961012498d6ebf9cdfd3de6
Ozan Gunalp <ozangunalp@gmail.com>
2025-04-23 14:55:53 +0200

    Graceful shutdown for named worker pools

--- smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/providers/connectors/WorkerPoolRegistry.java
+++ smallrye-reactive-messaging-provider/src/main/java/io/smallrye/reactive/messaging/providers/connectors/WorkerPoolRegistry.java
@@ -3,14 +3,16 @@ package io.smallrye.reactive.messaging.providers.connectors;
 import static io.smallrye.reactive.messaging.providers.i18n.ProviderExceptions.ex;
 import static io.smallrye.reactive.messaging.providers.i18n.ProviderLogging.log;
 import static io.smallrye.reactive.messaging.providers.i18n.ProviderMessages.msg;
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Priority;
@@ -32,6 +34,7 @@ import io.smallrye.mutiny.infrastructure.Infrastructure;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.smallrye.reactive.messaging.providers.helpers.Validation;
 import io.vertx.core.impl.ContextInternal;
+import io.vertx.core.impl.WorkerExecutorInternal;
 import io.vertx.mutiny.core.Context;
 import io.vertx.mutiny.core.Vertx;
 import io.vertx.mutiny.core.WorkerExecutor;
@@ -40,6 +43,10 @@ import io.vertx.mutiny.core.WorkerExecutor;
 public class WorkerPoolRegistry {
     public static final String WORKER_CONFIG_PREFIX = "smallrye.messaging.worker";
     public static final String WORKER_CONCURRENCY = "max-concurrency";
+    public static final String SHUTDOWN_TIMEOUT = "shutdown-timeout";
+    public static final String SHUTDOWN_CHECK_INTERVAL = "shutdown-check-interval";
+    public static final int DEFAULT_SHUTDOWN_TIMEOUT_MS = 60 * 1000; // 1 minute
+    public static final int DEFAULT_SHUTDOWN_CHECK_INTERVAL_MS = 5 * 1000; // 5 seconds
 
     @Inject
     Instance<ExecutionHolder> executionHolder;
@@ -47,18 +54,54 @@ public class WorkerPoolRegistry {
     @Inject
     Instance<Config> configInstance;
 
-    private final Map<String, Integer> workerConcurrency = new HashMap<>();
+    private final Map<String, WorkerPoolConfig> workerConfig = new HashMap<>();
     private final Map<String, WorkerExecutor> workerExecutors = new ConcurrentHashMap<>();
     private ExecutionHolder holder;
+
+    private volatile boolean closed = false;
 
     public void terminate(
             @Observes(notifyObserver = Reception.IF_EXISTS) @Priority(100) @BeforeDestroyed(ApplicationScoped.class) Object event) {
         if (!workerExecutors.isEmpty()) {
-            for (WorkerExecutor executor : workerExecutors.values()) {
-                if (Infrastructure.canCallerThreadBeBlocked()) {
-                    executor.closeAndAwait();
-                } else {
+            closed = true;
+            // In most cases this is called from the main thread, so we can block.
+            if (!Infrastructure.canCallerThreadBeBlocked()) {
+                for (WorkerExecutor executor : workerExecutors.values()) {
                     executor.closeAndForget();
+                }
+                return;
+            }
+            // Shutdown all worker executors
+            for (Map.Entry<String, WorkerExecutor> entry : workerExecutors.entrySet()) {
+                ((WorkerExecutorInternal) entry.getValue().getDelegate()).getPool().executor().shutdown();
+            }
+            long start = System.nanoTime();
+            boolean terminated = false;
+            int loop = 1;
+            long elapsed = 0;
+            while (!terminated) {
+                terminated = true;
+                for (Map.Entry<String, WorkerExecutor> entry : workerExecutors.entrySet()) {
+                    WorkerExecutor workerExecutor = entry.getValue();
+                    ExecutorService innerExecutor = ((WorkerExecutorInternal) workerExecutor.getDelegate()).getPool()
+                            .executor();
+                    WorkerPoolConfig poolConfig = workerConfig.get(entry.getKey());
+                    long timeout = poolConfig.shutdownTimeout().toNanos();
+                    long interval = poolConfig.shutdownCheckInterval().toNanos();
+                    log.debugf("Await termination loop: %s, remaining: %s", loop++, timeout - elapsed);
+                    try {
+                        if (!innerExecutor.awaitTermination(Math.min(timeout, interval), NANOSECONDS)) {
+                            elapsed = System.nanoTime() - start;
+                            if (elapsed >= timeout) {
+                                innerExecutor.shutdownNow();
+                                workerExecutor.closeAndAwait();
+                            } else {
+                                terminated = false; // Still waiting
+                            }
+                        }
+                    } catch (InterruptedException ignored) {
+                        terminated = false;
+                    }
                 }
             }
         }
@@ -74,6 +117,9 @@ public class WorkerPoolRegistry {
     }
 
     public <T> Uni<T> executeWork(Context msgContext, Uni<T> uni, String workerName, boolean ordered) {
+        if (closed) {
+            throw new RejectedExecutionException("WorkerPoolRegistry is being shut down");
+        }
         if (holder == null) {
             throw new UnsupportedOperationException("@Blocking disabled");
         }
@@ -142,15 +188,15 @@ public class WorkerPoolRegistry {
         if (workerExecutors.containsKey(workerName)) {
             return workerExecutors.get(workerName);
         }
-        if (workerConcurrency.containsKey(workerName)) {
+        if (workerConfig.containsKey(workerName)) {
             WorkerExecutor executor = workerExecutors.get(workerName);
             if (executor == null) {
                 synchronized (this) {
                     executor = workerExecutors.get(workerName);
                     if (executor == null) {
-                        executor = holder.vertx().createSharedWorkerExecutor(workerName,
-                                workerConcurrency.get(workerName));
-                        log.workerPoolCreated(workerName, workerConcurrency.get(workerName));
+                        int poolSize = workerConfig.get(workerName).maxConcurrency();
+                        executor = holder.vertx().createSharedWorkerExecutor(workerName, poolSize);
+                        log.workerPoolCreated(workerName, poolSize);
                         workerExecutors.put(workerName, executor);
                     }
                 }
@@ -186,16 +232,24 @@ public class WorkerPoolRegistry {
                 throw ex.illegalArgumentForAnnotationNullOrBlank("@Blocking", className + "#" + method);
             }
 
-            // Validate @Blocking worker pool has configuration to define concurrency
-            String workerConfigKey = WORKER_CONFIG_PREFIX + "." + poolName + "." + WORKER_CONCURRENCY;
-            Optional<Integer> concurrency = configInstance.get().getOptionalValue(workerConfigKey, Integer.class);
-            if (!concurrency.isPresent()) {
-                throw ex.illegalArgumentForWorkerConfigKey("@Blocking", className + "#" + method,
-                        workerConfigKey);
-            }
-
-            workerConcurrency.put(poolName, concurrency.get());
+            workerConfig.put(poolName, getWorkerPoolConfig(className, method, poolName));
         }
+    }
+
+    private WorkerPoolConfig getWorkerPoolConfig(String className, String method, String poolName) {
+        // Validate @Blocking worker pool has configuration to define concurrency
+        Config config = configInstance.get();
+        String maxConcurrencyConfigKey = WORKER_CONFIG_PREFIX + "." + poolName + "." + WORKER_CONCURRENCY;
+        String shutdownTimeoutConfigKey = WORKER_CONFIG_PREFIX + "." + poolName + "." + SHUTDOWN_TIMEOUT;
+        String shutdownCheckIntervalConfigKey = WORKER_CONFIG_PREFIX + "." + poolName + "." + SHUTDOWN_CHECK_INTERVAL;
+        int maxConcurrency = config.getOptionalValue(maxConcurrencyConfigKey, Integer.class)
+                .orElseThrow(() -> ex.illegalArgumentForWorkerConfigKey("@Blocking", className + "#" + method,
+                        maxConcurrencyConfigKey));
+        int shutdownTimeout = config.getOptionalValue(shutdownTimeoutConfigKey, Integer.class)
+                .orElse(DEFAULT_SHUTDOWN_TIMEOUT_MS);
+        int shutdownCheckInterval = config.getOptionalValue(shutdownCheckIntervalConfigKey, Integer.class)
+                .orElse(DEFAULT_SHUTDOWN_CHECK_INTERVAL_MS);
+        return new WorkerPoolConfig(maxConcurrency, shutdownTimeout, shutdownCheckInterval);
     }
 
     private void defineWorker(Method method) {
