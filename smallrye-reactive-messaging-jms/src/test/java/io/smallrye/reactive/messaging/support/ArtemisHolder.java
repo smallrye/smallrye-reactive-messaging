e6070f93ae1ea0593ff25571c2f902559a29e112
Ozan Gunalp <ozangunalp@gmail.com>
2025-09-09 13:01:32 +0200

    Restart the server without wiping the data.
    This can cause duplicates in some cases, but saves the at-least-once property of the test.

--- smallrye-reactive-messaging-jms/src/test/java/io/smallrye/reactive/messaging/support/ArtemisHolder.java
+++ smallrye-reactive-messaging-jms/src/test/java/io/smallrye/reactive/messaging/support/ArtemisHolder.java
@@ -3,6 +3,7 @@ package io.smallrye.reactive.messaging.support;
 import java.nio.file.Paths;
 
 import org.apache.activemq.artemis.core.server.ActiveMQServer;
+import org.apache.activemq.artemis.core.server.Queue;
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ;
 import org.apache.commons.io.FileUtils;
 
@@ -20,9 +21,19 @@ class ArtemisHolder {
         }
     }
 
+    void restart() {
+        try {
+            embedded = new EmbeddedActiveMQ();
+            embedded.start();
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not start embedded ActiveMQ server", e);
+        }
+    }
+
     void stop() {
         try {
             embedded.stop();
+            embedded = null;
         } catch (Exception e) {
             throw new IllegalStateException("Could not stop embedded ActiveMQ server", e);
         }
