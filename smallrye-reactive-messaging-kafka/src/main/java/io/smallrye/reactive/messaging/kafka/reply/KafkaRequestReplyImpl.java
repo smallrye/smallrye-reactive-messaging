9901c4960671c6bb980535cb9be2e3970184c154
Ozan Gunalp <ozangunalp@gmail.com>
2026-01-07 11:46:33 +0100

    Message converter with failure handling (#3262)

--- smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/reply/KafkaRequestReplyImpl.java
+++ smallrye-reactive-messaging-kafka/src/main/java/io/smallrye/reactive/messaging/kafka/reply/KafkaRequestReplyImpl.java
@@ -74,7 +74,7 @@ public class KafkaRequestReplyImpl<Req, Rep> extends MutinyEmitterImpl<Req>
     private final Set<TopicPartition> waitForPartitions;
     private final boolean gracefulShutdown;
     private final Duration initialAssignmentTimeout;
-    private Function<Message<Rep>, Message<Rep>> replyConverter;
+    private Function<Message<Rep>, Uni<Message<Rep>>> replyConverter;
 
     public KafkaRequestReplyImpl(EmitterConfiguration config,
             long defaultBufferSize,
@@ -246,7 +246,8 @@ public class KafkaRequestReplyImpl<Req, Rep> extends MutinyEmitterImpl<Req>
                     return Uni.createFrom().item(m);
                 })
                 .onTermination().invoke(() -> pendingReplies.remove(correlationId))
-                .plug(multi -> replyConverter != null ? multi.map(f -> replyConverter.apply(f)) : multi);
+                .plug(multi -> replyConverter != null ? multi
+                        .onItem().transformToUniAndConcatenate(f -> replyConverter.apply(f)) : multi);
     }
 
     @Override
@@ -268,7 +269,7 @@ public class KafkaRequestReplyImpl<Req, Rep> extends MutinyEmitterImpl<Req>
                 .toUni();
     }
 
-    void setReplyConverter(Function<Message<Rep>, Message<Rep>> converterFunction) {
+    void setReplyConverter(Function<Message<Rep>, Uni<Message<Rep>>> converterFunction) {
         this.replyConverter = converterFunction;
     }
 
