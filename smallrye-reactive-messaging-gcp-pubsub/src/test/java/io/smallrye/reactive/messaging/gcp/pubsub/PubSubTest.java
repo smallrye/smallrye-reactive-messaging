34424beaf0cf877765e6d3cbe718b5c6015d93d9
Raphael Nascimento <raphasil@gmail.com>
2026-03-26 14:51:56 +0100

    Add OpenTelemetry support to PubSub configuration (#2946)
    * Bump google-cloud libraries-bom version to 26.55.0 and add smallrye-reactive-messaging-otel dependency
    
    * Add OpenTelemetry support to PubSub configuration and connectors
    
    * Tests for enabling OTel for gcp pubsub
    
    ---------
    
    Co-authored-by: Ozan Gunalp <ozangunalp@gmail.com>

--- smallrye-reactive-messaging-gcp-pubsub/src/test/java/io/smallrye/reactive/messaging/gcp/pubsub/PubSubTest.java
+++ smallrye-reactive-messaging-gcp-pubsub/src/test/java/io/smallrye/reactive/messaging/gcp/pubsub/PubSubTest.java
@@ -3,21 +3,24 @@ package io.smallrye.reactive.messaging.gcp.pubsub;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
-import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.UUID;
 import java.util.concurrent.Flow;
 
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.reactive.messaging.Message;
-import org.eclipse.microprofile.reactive.messaging.spi.Connector;
+import org.eclipse.microprofile.reactive.messaging.spi.ConnectorLiteral;
 import org.jboss.weld.environment.se.Weld;
 import org.jboss.weld.environment.se.WeldContainer;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import com.google.pubsub.v1.ProjectTopicName;
 import com.google.pubsub.v1.TopicName;
 
+import io.smallrye.config.SmallRyeConfigProviderResolver;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.reactive.messaging.test.common.config.MapBasedConfig;
 
@@ -25,27 +28,30 @@ public class PubSubTest extends PubSubTestBase {
 
     private WeldContainer container;
 
-    private static final String TOPIC = "pubsub-test";
+    private String topic;
 
     @BeforeEach
-    public void initTest() {
-        initConfiguration(TOPIC);
+    public void initTest(TestInfo testInfo) {
+        topic = testInfo.getTestMethod().map(Method::getName).orElse("") + "_" + UUID.randomUUID();
+        initConfiguration(topic);
         final Weld weld = baseWeld();
         weld.addBeanClass(ConsumptionBean.class);
-        addConfig(createSourceConfig(TOPIC, SUBSCRIPTION, PUBSUB_CONTAINER.getFirstMappedPort()));
+        addConfig(createSourceConfig(topic, SUBSCRIPTION, PUBSUB_CONTAINER.getFirstMappedPort()));
         container = weld.initialize();
     }
 
     @AfterEach
     public void afterEach() {
+        if (container != null) {
+            PubSubManager manager = container.select(PubSubManager.class).get();
+            deleteTopicIfExists(manager, topic);
+            container.shutdown();
+        }
+        SmallRyeConfigProviderResolver.instance().releaseConfig(ConfigProvider.getConfig());
         clear();
-        PubSubManager manager = container.select(PubSubManager.class).get();
-        deleteTopicIfExists(manager, TOPIC);
-        container.shutdown();
     }
 
     @Test
-    @Disabled("Failing on CI - to be investigated")
     public void testSourceAndSink() {
         final ConsumptionBean consumptionBean = container.select(ConsumptionBean.class).get();
 
@@ -53,15 +59,15 @@ public class PubSubTest extends PubSubTestBase {
         final PubSubManager manager = container.select(PubSubManager.class).get();
         await().until(() -> manager
                 .topicAdminClient(config)
-                .listTopicSubscriptions((TopicName) ProjectTopicName.of(PROJECT_ID, TOPIC))
+                .listTopicSubscriptions((TopicName) ProjectTopicName.of(PROJECT_ID, topic))
                 .getPage()
                 .getPageElementCount() > 0);
 
-        send("Hello-0", TOPIC);
+        send("Hello-0", topic);
         await().until(() -> consumptionBean.getMessages().size() == 1);
         assertThat(consumptionBean.getMessages().get(0)).isEqualTo("Hello-0");
         for (int i = 1; i < 11; i++) {
-            send("Hello-" + i, TOPIC);
+            send("Hello-" + i, topic);
         }
         await().until(() -> consumptionBean.getMessages().size() == 11);
         assertThat(consumptionBean.getMessages()).allSatisfy(s -> assertThat(s).startsWith("Hello-"));
@@ -84,17 +90,7 @@ public class PubSubTest extends PubSubTestBase {
     }
 
     private PubSubConnector getConnector() {
-        return container.select(PubSubConnector.class, new Connector() {
-            @Override
-            public Class<? extends Annotation> annotationType() {
-                return Connector.class;
-            }
-
-            @Override
-            public String value() {
-                return PubSubConnector.CONNECTOR_NAME;
-            }
-        }).get();
+        return container.select(PubSubConnector.class, ConnectorLiteral.of(PubSubConnector.CONNECTOR_NAME)).get();
     }
 
 }
